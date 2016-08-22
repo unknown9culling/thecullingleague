8dfaba469dea942050c3f6093e4901369bfcd6f8
Josh B <unknown9culling@gmail.com>
2016-08-22 16:06:52 -0700

    :+1: Added voting system

--- imports/api/games.js
+++ imports/api/games.js
@@ -1,10 +1,16 @@
 import { Mongo } from 'meteor/mongo'
 import { Tournaments } from './tournaments'
+import { Votes } from './votes'
 
 export const Games = new Mongo.Collection('games')
 
 Games.helpers({
   tournament() {
     return Tournaments.findOne({_id: this.tournamentId})
+  },
+  winner() {
+    return Votes.findOne({
+      gameId: this._id
+    }, {sort: {_id:-1}}).player
   }
 })
