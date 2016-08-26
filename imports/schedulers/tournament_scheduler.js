5f679763d381b9932518d8a936808c19455370e5
Josh B <unknown9culling@gmail.com>
2016-08-25 22:36:19 -0700

    Adjusted timings

--- imports/schedulers/tournament_scheduler.js
+++ imports/schedulers/tournament_scheduler.js
@@ -6,8 +6,8 @@ var scheduleTournaments = function() {
   if(count == 0) {
     // schedule an upcoming tournament
     Tournaments.insert({
-      startDate: moment().add(2, 'minutes').toDate(),
-      endRegister: moment().add(1, 'minutes').toDate(),
+      startDate: moment().add(60, 'minutes').toDate(),
+      endRegister: moment().add(70, 'minutes').toDate(),
       tournamentName: 'Weekly ManTracker.co Tournament',
       tournamentDescription: 'Fight to the death in our ManTracker hosted weekly tournament. Every week, players will fight to win by joining the weekly tournament and earning points when they win. Play now!',
       slots: 64,
